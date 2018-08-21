9bdb54296ad6c931b582e7812178ff541fa8ca32
Richard Garner <richardgarner@mac.com>
2018-08-21 16:45:40 -0500

    set 2 for both jQ and DOM complete

--- DOM side/DOM.js
+++ DOM side/DOM.js
@@ -7,3 +7,14 @@ body.appendChild(button)
 button.addEventListener("click", function(){
     window.alert("Well, arn't you lookin' cute today. ;)")
 })
+
+
+let submit = document.getElementById('submit');
+submit.addEventListener("click", function(e){
+    e.preventDefault()
+    let text = document.getElementById('formText');
+    let value = text.value;
+    console.log(value)
+    window.alert(value)
+
+})
--- jQuery Side/jQ.js
+++ jQuery Side/jQ.js
@@ -2,6 +2,12 @@ let body = document.body;
 let button = $("<button>" + "Click me, Fool!" + "</button>").appendTo(body);
 $(button).appendTo(body);
 
-$(button).click(function(){
+$(button).click(function () {
     window.alert("Well, arn't you lookin' cute today. ;)")
 })
+
+$("#submit").click(function (e) {
+    e.preventDefault()
+    let wordz = $("#formText").val()
+    window.alert(wordz)
+});
